344c6696f0fb04f5c63253eae3ce5db263e4718c
carlos <carlos@ef848c78-7784-6640-bb0b-7af4aec43c47>
2009-10-08 20:20:33 +0000

    - CHG: Alterado interadmin_query() para funcionar quando tabela de tipos que se está buscando não é do mesmo cliente.

--- branches/release-1.6.0/7.lib.php
+++ branches/release-1.6.0/7.lib.php
@@ -923,7 +923,7 @@ function interadmin_query($sql, $sql_db = "", $sql_debug = FALSE, $numrows = NUL
 		// Com Alias
 		foreach ($out[1] as $key=>$value) {
 			$alias = $out[2][$key];
-			if (strpos($value, $db_prefix . '_tipos') !== false) {
+			if (strpos($value, '_tipos') === (strlen($value) - strlen('_tipos'))) {
 				$sql_where = str_replace("WHERE ","WHERE (" . $alias . ".mostrar<>'' OR " . $alias . ".mostrar IS NULL) AND (" . $alias . ".deleted_tipo='' OR " . $alias . ".deleted_tipo IS NULL) AND ", $sql_where);
 			} elseif (strpos($value, $db_prefix . $lang->prefix . '_arquivos')!==false || strpos($value, $db_prefix . '_arquivos') !== false) {
 				$sql_where = str_replace("WHERE ","WHERE " . $alias . ".mostrar<>'' AND (" . $alias . ".deleted='' OR " . $alias . ".deleted IS NULL) AND ", $sql_where);
