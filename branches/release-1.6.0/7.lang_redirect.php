5f9a0d02a7f74085568c3f114847498dbe7df4f7
carlos <carlos@ef848c78-7784-6640-bb0b-7af4aec43c47>
2010-03-12 20:06:38 +0000

    - CHG: Branch já foi reintegrado.

--- branches/release-1.6.0/7.lang_redirect.php
+++
@@ -1,8 +0,0 @@
-<?
-$path=$SCRIPT_NAME;
-if(!$path)$path=$REQUEST_URI;
-$dirname=dirname($path);
-$dirname=substr($dirname,strrpos($dirname,"/"));
-$basename=basename($path);
-include "../../site".$dirname."/".$basename;
-?>
