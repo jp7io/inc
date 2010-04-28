d4e22378e6692622b977125a5ee5181a034729ad
carlos <carlos@ef848c78-7784-6640-bb0b-7af4aec43c47>
2010-04-28 19:48:48 +0000

    - CHG: Tag 1.6.4 - Retirada de senhas hardcode, $tipoObj agora utiliza classe do cliente, e suporte para a classe Jp7_Date nas funções antigas.

---
+++ tags/release-1.6.4/7.preview.php
@@ -0,0 +1,23 @@
+<? if ($s_session['preview'] || $config->server->type == InterSite::QA) { // Precisa ser Global ?>
+	<?
+	if ($config->server->type == InterSite::QA) {
+		$S = "QA";
+	}
+	if ($s_session['preview']) {
+		if ($config->server->type == InterSite::QA) {
+			$S .= " - " . strtoupper("Preview");
+		} else {
+			$S = strtoupper("Preview");
+		}
+	}
+	?>
+	<script>
+	function interadmin_preview(){
+		if(confirm("Deseja sair do modo PREVIEW e ir para o modo PUBLICADO?")) location='http://<?= $HTTP_HOST ?>/<?= $c_path ?>visualizar.php?redirect='+location.toString()
+	}
+	</script>
+	<div class="preview_type" style="left:0px;border-width:0px 1px 1px 0px;background:#FFCC00;filter:alpha(opacity=50);z-index:1000"><?= $S ?></div>
+	<div class="preview_type" style="right:0px;border-width:0px 0px 1px 1px;background:#FFCC00;filter:alpha(opacity=50);z-index:1000"><?= $S ?></div>
+	<div class="preview_type" style="left:0px;border-width:0px 1px 1px 0px;z-index:1001"<? /* onclick="interadmin_preview()"*/ ?>><?= $S ?></div>
+	<div class="preview_type" style="right:0px;border-width:0px 0px 1px 1px;z-index:1001"<? /* onclick="interadmin_preview()"*/ ?>><?= $S ?></div>
+<? } ?>
