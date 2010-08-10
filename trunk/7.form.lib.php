b09bbcab120b367c93a0e294459e65f32917ad1e
lucas <lucas@ef848c78-7784-6640-bb0b-7af4aec43c47>
2010-08-10 18:39:10 +0000

    - CHG: Ajustado colunas do campo tipo "file" e a imagem exibida ao lado.

--- trunk/7.form.lib.php
+++ trunk/7.form.lib.php
@@ -147,12 +147,12 @@ function interadmin_returnCampo($campo){
 			echo "".
 			"<tr>\r\n".
 				$_th.
-				"<td colspan=\"2\">
+				"<td>
 					<input type=\"file\" name=\"".$campo."[".$j."]\" value=\"".$valor."\" maxlength=\"255\"".$readonly." class=\"inputs_width\" />
 					<input type=\"hidden\" name=\"".$campo."[".$j."]\" value=\"".$valor."\" maxlength=\"255\"".$readonly." />
 				</td>\r\n";
 				if ($campo_array['thumb']) {
-					echo "<td><img src=\"" . $campo_array['thumb'] . "\" alt=\"\" class=\"file_thumb\" /></td>\r\n";
+					echo "<td colspan=\"2\"><img src=\"" . $campo_array['thumb'] . "\" alt=\"\" class=\"file_thumb\" /></td>\r\n";
 				}
 			echo "</tr>\r\n";
 		}elseif(strpos($campo,"date_")===0){
