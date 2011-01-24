fabd3d8463e467174e996e1bbf32fb4b9c51da87
carlos <carlos@ef848c78-7784-6640-bb0b-7af4aec43c47>
2011-01-24 17:35:49 +0000

    - NEW: Adicionada função jp7_absolute_path(). Issue: TI-25

--- branches/templates/7.lib.php
+++ branches/templates/7.lib.php
@@ -2616,3 +2616,19 @@ function jp7_xml_encode($data, $options = array()) {
 	return $xml;
 }
 
+function jp7_absolute_path($path) {
+	$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
+    $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+    $absolutes = array();
+    foreach ($parts as $part) {
+        if ('.' == $part) {
+        	continue;
+		}
+        if ('..' == $part) {
+            array_pop($absolutes);
+        } else {
+            $absolutes[] = $part;
+        }
+    }
+    return DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $absolutes);
+}
