be3fecd9ac5e8a8f59268cc2fb2877f1732f9af8
carlos <carlos@jp7.com.br>
2011-03-03 14:32:56 +0000

    - CHG: Inserido doctype HTML5.

--- 7.head.php
+++ 7.head.php
@@ -18,7 +18,11 @@ if ($seo_baseurl) {
 
 $c_build = interadmin_get_version($config->name_id, '{build}');
 ?>
+<?php if ($config->html5) { ?>
+<!DOCTYPE HTML>
+<?php } else { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php } ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
