c6be5fa8b64ae3826bcc5cdbbad7be9118144f56
carlos <carlos@jp7.com.br>
2011-08-26 14:42:06 +0000

    - NEW: Inserindo variável para adicionar namespace do Facebook à tag HTML.

--- 7.head.php
+++ 7.head.php
@@ -25,7 +25,7 @@ if ($seo_baseurl) {
 <?php } else { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php } ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml"<?php echo $config->html_facebook_namespace ? ' xmlns:fb="http://www.facebook.com/2008/fbml"' : ''; ?>>
 <head>
 <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
 <meta name="language" content="<?php echo $lang->lang; ?>" />
