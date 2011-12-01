0415a890fbad12839c5ada43be8168ddb03f29cf
carlos <carlos@jp7.com.br>
2011-12-01 13:02:31 +0000

    - CHG: Substituindo short_open_tags

--- 7.preview.php
+++ 7.preview.php
@@ -1,5 +1,5 @@
-<? if ($s_session['preview'] || $config->server->type == InterSite::QA) { // Precisa ser Global ?>
-	<?
+<?php if ($s_session['preview'] || $config->server->type == InterSite::QA) { // Precisa ser Global ?>
+	<?php
 	if ($config->server->type == InterSite::QA) {
 		$S = "QA";
 	}
@@ -13,11 +13,11 @@
 	?>
 	<script>
 	function interadmin_preview(){
-		if(confirm("Deseja sair do modo PREVIEW e ir para o modo PUBLICADO?")) location='http://<?= $HTTP_HOST ?>/<?= $c_path ?>visualizar.php?redirect='+location.toString()
+		if(confirm("Deseja sair do modo PREVIEW e ir para o modo PUBLICADO?")) location='http://<?php echo $_SERVER['HTTP_HOST'] ?>/<?php echo $c_path ?>visualizar.php?redirect='+location.toString()
 	}
 	</script>
-	<div class="preview_type" style="left:0px;border-width:0px 1px 1px 0px;background:#FFCC00;filter:alpha(opacity=50);z-index:1000"><?= $S ?></div>
-	<div class="preview_type" style="right:0px;border-width:0px 0px 1px 1px;background:#FFCC00;filter:alpha(opacity=50);z-index:1000"><?= $S ?></div>
-	<div class="preview_type" style="left:0px;border-width:0px 1px 1px 0px;z-index:1001"<? /* onclick="interadmin_preview()"*/ ?>><?= $S ?></div>
-	<div class="preview_type" style="right:0px;border-width:0px 0px 1px 1px;z-index:1001"<? /* onclick="interadmin_preview()"*/ ?>><?= $S ?></div>
-<? } ?>
+	<div class="preview_type" style="left:0px;border-width:0px 1px 1px 0px;background:#FFCC00;filter:alpha(opacity=50);z-index:1000"><?php echo $S ?></div>
+	<div class="preview_type" style="right:0px;border-width:0px 0px 1px 1px;background:#FFCC00;filter:alpha(opacity=50);z-index:1000"><?php echo $S ?></div>
+	<div class="preview_type" style="left:0px;border-width:0px 1px 1px 0px;z-index:1001"<?php /* onclick="interadmin_preview()"*/ ?>><?php echo $S ?></div>
+	<div class="preview_type" style="right:0px;border-width:0px 0px 1px 1px;z-index:1001"<?php /* onclick="interadmin_preview()"*/ ?>><?php echo $S ?></div>
+<?php } ?>
