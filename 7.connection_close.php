02461383fb198c2c2051100d99e499ca4a2f9e16
carlos <carlos@jp7.com.br>
2012-05-30 15:54:50 +0000

    - CHG: Mudanças para permitir iniciar DB antes da sessão para poder usar sessão no DB.

--- 7.connection_close.php
+++ 7.connection_close.php
@@ -5,9 +5,11 @@ if ($jp7_cache instanceof FileCache) {
 if ($debugger instanceof Jp7_Debugger) {
 	$debugger->showToolbar(); // Only called when cache is done, avoiding debug from being cached
 }
+/*
 if ($db instanceof ADOConnection) {
 	$db->Close();
 }
+*/
 //asort($GLOBALS['sql_execute_queries']);
 //krumo($GLOBALS['sql_execute_count']);
 //krumo($GLOBALS['sql_execute_queries']);
