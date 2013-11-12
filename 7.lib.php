759ab879ed313e7c41ec14d7641c18edae795a38
carlos <carlos@jp7.com.br>
2013-11-12 13:35:50 +0000

    - BUG: Bug do PHP, open_basedir impede o CURL de seguir redirects.

--- 7.lib.php
+++ 7.lib.php
@@ -3116,20 +3116,61 @@ function array_move_key($array, $key1, $key2, $pos = 1) {
 	return array_combine($keys, $values);
 }
 
+function curl_exec_follow($ch, /*int*/ $maxredirect = null) {
+	$mr = $maxredirect === null ? 5 : intval($maxredirect);
+	if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+	} else {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+		if ($mr > 0) {
+			$newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+
+			$rch = curl_copy_handle($ch);
+			curl_setopt($rch, CURLOPT_HEADER, true);
+			curl_setopt($rch, CURLOPT_NOBODY, true);
+			curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+			curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
+			do {
+				curl_setopt($rch, CURLOPT_URL, $newurl);
+				$header = curl_exec($rch);
+				if (curl_errno($rch)) {
+					$code = 0;
+				} else {
+					$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+					if ($code == 301 || $code == 302) {
+						preg_match('/Location:(.*?)\n/', $header, $matches);
+						$newurl = trim(array_pop($matches));
+					} else {
+						$code = 0;
+					}
+				}
+			} while ($code && --$mr);
+			curl_close($rch);
+			if (!$mr) {
+				if ($maxredirect === null) {
+					trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING);
+				} else {
+					$maxredirect = 0;
+				}
+				return false;
+			}
+			curl_setopt($ch, CURLOPT_URL, $newurl);
+		}
+	}
+	return curl_exec($ch);
+}
+
 function curl_get_contents($url, $options = array()) {
 	$ch = curl_init();
 	$timeout = 15;
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	if (ini_get('open_basedir') != '') {
-		throw new Exception('CURL não segue redirects com open_basedir ligado. Desligue o open_basedir.');
-	}
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 	if ($options['header']) {
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $options['header']);	
 	}
-	$data = curl_exec($ch);
+	$data = curl_exec_follow($ch, 20);
 	$info = curl_getinfo($ch);
 	curl_close($ch);
 	if ($info['http_code'] != 200) {
