80a2da99e265f4cb81552a64067403a51a6f13b3
Carlos Rodrigues <carlos@jp7.com.br>
2016-11-22 15:55:41 -0200

    Add EncryptTest

---
+++ tests/EncryptTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class EncryptTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $data = [];
+        foreach (range(1,5) as $_) {
+            $data[] = mt_rand();
+        }
+        $encrypted = jp7_encrypt(implode('&', $data));
+        $this->assertEquals($data, explode('&', jp7_decrypt($encrypted)));
+    }
+}
