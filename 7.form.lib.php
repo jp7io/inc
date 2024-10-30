263357b820239f7f93551ba7726306ffe6eb1eae
JP Barbosa <jp@jp7.io>
2024-10-29 23:59:14 -0300

    Rename deleted to deleted_at

--- 7.form.lib.php
+++ 7.form.lib.php
@@ -294,13 +294,13 @@ function interadmin_combo($current_id, $parent_type_id_2, $nivel = 0, $prefix = 
     }
     // Loop
     if ($select_tabela) {
-        $sql = 'SELECT id,varchar_key,deleted'.$select_campos_2.' FROM '.$db_prefix.'_'.$select_tabela.
+        $sql = 'SELECT id,varchar_key,deleted_at'.$select_campos_2.' FROM '.$db_prefix.'_'.$select_tabela.
         ' WHERE type_id='.$parent_type_id_2.
         $sql_where.
         " AND (deleted='' OR deleted IS NULL) ".
         ' ORDER BY varchar_key';
     } else {
-        $sql = 'SELECT id,varchar_key,deleted'.$select_campos_2.' FROM '.$db_prefix.(($select_lang) ? $lang->prefix : '').
+        $sql = 'SELECT id,varchar_key,deleted_at'.$select_campos_2.' FROM '.$db_prefix.(($select_lang) ? $lang->prefix : '').
         ' WHERE type_id='.$parent_type_id_2.
         $sql_where.
         " AND (deleted='' OR deleted IS NULL) ".
